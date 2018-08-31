9c2c3cace29bec9970450f218904792410db27eb
KENNETH LOW <kennethml.kml@gmail.com>
2018-08-31 23:29:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 function shout(string){
   return string.toUpperCase()
 }
+
+function whisper(string){
+  return string.lowease()
+}
+
+
+describe('whisper(string)', function() {
+  it('receives one argument and returns it in all lowercase', function() {
+    expect(whisper('HELLO')).toEqual('hello')
+  })
+})
